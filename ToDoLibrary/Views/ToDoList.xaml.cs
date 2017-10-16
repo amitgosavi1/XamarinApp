69b8dcac982d0d4732879a3c693a26cd2b29022f
amitgosavi1 <amit.gosavi1@globallogic.com>
2017-10-16 16:41:18 +0530

    Added data model & displayed list with dummy data.

--- ToDoLibrary/Views/ToDoList.xaml.cs
+++ ToDoLibrary/Views/ToDoList.xaml.cs
@@ -1,15 +1,49 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using System.Diagnostics;
 using Xamarin.Forms;
+using ToDoLibrary.Models;
+using System.Collections.ObjectModel;
 
 namespace ToDoLibrary.Views
 {
     public partial class ToDoList : ContentPage
     {
-        public ToDoList() {
+         ObservableCollection<TodoItem> items = new ObservableCollection<TodoItem>();
+                public ToDoList() {
 
             InitializeComponent();
         }
+
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+            TodoItem item1 = new TodoItem()
+            {
+                ID = 1,
+                 Name = "Item 1",
+                Notes = "Hello",
+                Done = false
+
+            };
+            items.Add(item1);
+            TodoItem item2 = new TodoItem()
+            {
+                ID = 2,
+                Name = "Item 2",
+                Notes = "Hello World",
+                Done = false
+
+            };
+
+            items.Add(item2);
+            listItems.ItemsSource = items;
+
+
+        } 
     }
+
+
+
+
 }
